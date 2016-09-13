030a7356aef43f2072e6c834782686041c2e936a
vvasiloud <vvasiloudis@gmail.com>
2016-09-13 08:05:27 +0300

    Gateway service initial commit

---
+++ gateway/src/main/java/org/vvasiloud/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package org.vvasiloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Aeon on 14/8/2016.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
