45c8c9fdf7b8981908e4d30bdb7e1ca6b4197c97
vvasiloud <vvasiloudis@gmail.com>
2016-09-13 08:04:08 +0300

    Discovery service initial commit

---
+++ registry/src/main/java/org/vvasiloud/registry/RegistryApplication.java
@@ -0,0 +1,17 @@
+package org.vvasiloud.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Aeon on 13/8/2016.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
