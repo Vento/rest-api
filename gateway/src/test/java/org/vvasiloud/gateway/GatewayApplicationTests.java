df27669ba8823af782ad5a243116c84cc442c3cf
vvasiloud <vvasiloudis@gmail.com>
2016-11-03 20:30:51 +0200

    Added basic context test for gateway service

---
+++ gateway/src/test/java/org/vvasiloud/gateway/GatewayApplicationTests.java
@@ -0,0 +1,19 @@
+package org.vvasiloud.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Aeon on 30/10/2016.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GatewayApplication.class)
+public class GatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
