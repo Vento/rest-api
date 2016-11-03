d7b4defc6f7cb56d88267a197c40c2c865ec2824
vvasiloud <vvasiloudis@gmail.com>
2016-11-03 20:31:10 +0200

    Added email on user profile

--- profile-service/src/main/java/org/vvasiloud/service/profile/domain/User.java
+++ profile-service/src/main/java/org/vvasiloud/service/profile/domain/User.java
@@ -1,5 +1,6 @@
 package org.vvasiloud.service.profile.domain;
 
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotNull;
 
@@ -16,6 +17,10 @@ public class User {
     @Length(min = 8, max = 40)
     private String password;
 
+    @NotNull
+    @Email
+    private String email;
+
     public String getUsername() {
         return username;
     }
@@ -30,5 +35,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
