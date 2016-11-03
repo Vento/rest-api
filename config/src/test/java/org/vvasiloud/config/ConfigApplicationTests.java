daf806772dddd77703e6d008fbb668dfe124e331
vvasiloud <vvasiloudis@gmail.com>
2016-11-03 20:30:24 +0200

    Added basic context test for config service

---
+++ config/src/test/java/org/vvasiloud/config/ConfigApplicationTests.java
@@ -0,0 +1,18 @@
+package org.vvasiloud.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Aeon on 30/10/2016.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConfigApplication.class)
+public class ConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
