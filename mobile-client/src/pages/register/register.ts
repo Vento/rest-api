1158c7a2a478a34686daf93778dbafd7a03d1759
vvasiloud <vvasiloudis@gmail.com>
2016-11-28 00:04:04 +0200

    Registration page changes

--- mobile-client/src/pages/register/register.ts
+++ mobile-client/src/pages/register/register.ts
@@ -5,20 +5,38 @@ import { ToastController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { Dashboard } from '../dashboard/dashboard';
 import { ProfileService } from '../../providers/profile-service/profile-service';
+import { AuthService } from '../../providers/auth-service/auth-service';
+import { ProfileStorage } from '../../providers/profile-storage/profile-storage';
+import { AuthStorage } from '../../providers/auth-storage/auth-storage';
+import { User } from './user.interface';
+
+import {Observable} from 'rxjs/Observable';
+
+
 
 @Component({
   selector: 'page-register',
-  templateUrl: 'register.html',
-  providers: [ProfileService]
+  templateUrl: 'register.html'
 })
 export class Register {
-  register: {email?: string, username?: string, password?: string, passwordRepeat?: string} = {};
+  public user: User;
   submitted = false;
-  loader:any;
-
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController,public toastCtrl: ToastController,
-  public profileService: ProfileService) {
-    this.loader = this.loadingCtrl.create({content: "Please wait..."}); 
+  loader: any;
+  
+  constructor(public navCtrl: NavController,
+              private loadingCtrl: LoadingController,
+              private toastCtrl: ToastController,
+              private profileService: ProfileService,
+              private authService: AuthService,
+              private profileStorage: ProfileStorage,
+              private authStorage: AuthStorage) {
+    this.loader = this.loadingCtrl.create({ content: "Please wait..." }); 
+    this.user = {
+      email: '',
+      username: '',
+      password: '',
+      passwordRepeat: ''
+    };
   }
 
   onRegister(form) {
@@ -26,10 +44,29 @@ export class Register {
 
     if (form.valid) {
       this.presentLoading();
-      this.profileService.createProfile(this.register).subscribe(
-            data => {          
-              this.dismissLoading();     
-              this.navCtrl.setRoot(Dashboard);
+      this.profileService.createProfile(this.user).subscribe(
+        profileData => {    
+          
+          this.presentToast("Registration Success!");
+
+              this.authService.requestToken(this.user).subscribe(
+                    authData => {          
+                      this.authStorage.setAccessToken(authData.access_token);
+                      this.authStorage.setRefreshToken(authData.refresh_token);
+                      this.authStorage.setTokenExpiration(authData.expires_in);                      
+                      this.profileStorage.setProfile(profileData);
+
+                      console.log(authData);
+                      console.log(profileData);
+                      this.dismissLoading();     
+                      this.navCtrl.setRoot(Dashboard);
+                    },
+                    err  => {
+                      this.dismissLoading(); 
+                      this.presentToast(<any>err);
+                    }
+                    
+                )
             },
             err  => {
               this.dismissLoading(); 
@@ -37,12 +74,13 @@ export class Register {
             }
             
         )
+
     }
   }
 
   presentLoading() {
     this.loader.present();
-}
+  }
 
   dismissLoading(){
       this.loader.dismissAll();
