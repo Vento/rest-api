ae495053837c41d2a8de84f02fb5b9cbd489787a
vvasiloud <vvasiloudis@gmail.com>
2016-12-31 22:55:32 +0200

    Minor script change

--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -10,41 +10,41 @@ export DOCKER_PROFILE=$DOCKER_USERNAME/vento-profile
 export DOCKER_REGISTRY=$DOCKER_USERNAME/vento-registry
 export COMMIT=${TRAVIS_COMMIT::6}
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-ls -la
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker --version
 	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	
 	echo ${DOCKER_AUTH}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_AUTH}:${COMMIT} -f ../auth-service/Dockefile
+	docker build -t ${DOCKER_AUTH}:${COMMIT} -f ../auth-service .
 	docker tag ${DOCKER_AUTH}:${COMMIT} ${DOCKER_AUTH}:${TAG} && docker push ${DOCKER_AUTH}:${TAG}
 
 	echo ${DOCKER_CONFIG}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_CONFIG}:${COMMIT} -f ../config/Dockefile
+	docker build -t ${DOCKER_CONFIG}:${COMMIT} -f ../config .
 	docker tag ${DOCKER_CONFIG}:${COMMIT} ${DOCKER_CONFIG}:${TAG} && docker push ${DOCKER_CONFIG}:${TAG}
 
 	echo ${DOCKER_GATEWAY}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_GATEWAY}:${COMMIT} -f ../gateway/Dockefile
+	docker build -t ${DOCKER_GATEWAY}:${COMMIT} -f ../gateway .
 	docker tag ${DOCKER_GATEWAY}:${COMMIT} ${DOCKER_GATEWAY}:${TAG} && docker push ${DOCKER_GATEWAY}:${TAG}
 
 	echo ${DOCKER_LOGGING}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_LOGGING}:${COMMIT} -f ../logging/Dockefile
+	docker build -t ${DOCKER_LOGGING}:${COMMIT} -f ../logging .
 	docker tag ${DOCKER_LOGGING}:${COMMIT} ${DOCKER_LOGGING}:${TAG} && docker push ${DOCKER_LOGGING}:${TAG}
 
 	echo ${DOCKER_MATCH}:${TAG}:${COMMIT}	
-	docker build -t ${DOCKER_MATCH}:${COMMIT} -f ../match-service/Dockefile
+	docker build -t ${DOCKER_MATCH}:${COMMIT} -f ../match-service .
 	docker tag ${DOCKER_MATCH}:${COMMIT} ${DOCKER_MATCH}:${TAG} && docker push ${DOCKER_MATCH}:${TAG}
 
 	echo ${DOCKER_MONITORING}:${TAG}:${COMMIT}		
-	docker build -t ${DOCKER_MONITORING}:${COMMIT} -f ../monitoring/Dockefile
+	docker build -t ${DOCKER_MONITORING}:${COMMIT} -f ../monitoring .
 	docker tag ${DOCKER_MONITORING}:${COMMIT} ${DOCKER_MONITORING}:${TAG} && docker push ${DOCKER_MONITORING}:${TAG}
 
 	echo ${DOCKER_PROFILE}:${TAG}:${COMMIT}	
-	docker build -t ${DOCKER_PROFILE}:${COMMIT} -f ../profile-service/Dockefile
+	docker build -t ${DOCKER_PROFILE}:${COMMIT} -f ../profile-service .
 	docker tag ${DOCKER_PROFILE}:${COMMIT} ${DOCKER_PROFILE}:${TAG} && docker push ${DOCKER_PROFILE}:${TAG}
 
 	echo ${DOCKER_REGISTRY}:${TAG}:${COMMIT}		
-	docker build -t ${DOCKER_REGISTRY}:${COMMIT} -f ../registry/Dockefile
+	docker build -t ${DOCKER_REGISTRY}:${COMMIT} -f ../registry .
 	docker tag ${DOCKER_REGISTRY}:${COMMIT} ${DOCKER_REGISTRY}:${TAG} && docker push ${DOCKER_REGISTRY}:${TAG}
 	
 fi
