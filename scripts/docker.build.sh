1fa60b68fbfb6584d5ecd68de3a395cf77484aa9
vvasiloud <vvasiloudis@gmail.com>
2016-12-31 20:35:19 +0200

    changes of enviroment variables for docker, removed deprecated docker_email param

--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
+export TAG = `if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker --version
-	docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	
-	docker build -t ${DOCKER_AUTH}:${TRAVIS_COMMIT} ../auth-service
-	docker tag -f ${DOCKER_AUTH}:${TRAVIS_COMMIT} ${DOCKER_AUTH}:latest && docker push ${DOCKER_AUTH}:latest
+	echo $DOCKER_AUTH:$TAG:$COMMIT
+	docker build -t ${DOCKER_AUTH}:${COMMIT} ../auth-service
+	docker tag ${DOCKER_AUTH}:${COMMIT} ${DOCKER_AUTH}:${TAG} && docker push ${DOCKER_AUTH}:${TAG}
 
-	docker build -t ${DOCKER_CONFIG}:${TRAVIS_COMMIT} ../config
-	docker tag -f ${DOCKER_CONFIG}:${TRAVIS_COMMIT} ${DOCKER_CONFIG}:latest && docker push ${DOCKER_CONFIG}:latest
+	echo $DOCKER_CONFIG:$TAG:$COMMIT
+	docker build -t ${DOCKER_CONFIG}:${COMMIT} ../config
+	docker tag ${DOCKER_CONFIG}:${COMMIT} ${DOCKER_CONFIG}:${TAG} && docker push ${DOCKER_CONFIG}:${TAG}
 
-	docker build -t ${DOCKER_GATEWAY}:${TRAVIS_COMMIT} ../gateway
-	docker tag -f ${DOCKER_GATEWAY}:${TRAVIS_COMMIT} ${DOCKER_GATEWAY}:latest && docker push ${DOCKER_GATEWAY}:latest
+	echo $DOCKER_GATEWAY:$TAG:$COMMIT
+	docker build -t ${DOCKER_GATEWAY}:${COMMIT} ../gateway
+	docker tag ${DOCKER_GATEWAY}:${COMMIT} ${DOCKER_GATEWAY}:${TAG} && docker push ${DOCKER_GATEWAY}:${TAG}
 
-	docker build -t ${DOCKER_LOGGING}:${TRAVIS_COMMIT} ../logging
-	docker tag -f ${DOCKER_LOGGING}:${TRAVIS_COMMIT} ${DOCKER_LOGGING}:latest && docker push ${DOCKER_LOGGING}:latest
-	
-	docker build -t ${DOCKER_MATCH}:${TRAVIS_COMMIT} ../match-service
-	docker tag -f ${DOCKER_MATCH}:${TRAVIS_COMMIT} ${DOCKER_MATCH}:latest && docker push ${DOCKER_MATCH}:latest
-	
-	docker build -t ${DOCKER_MONITORING}:${TRAVIS_COMMIT} ../monitoring
-	docker tag -f ${DOCKER_MONITORING}:${TRAVIS_COMMIT} ${DOCKER_MONITORING}:latest && docker push ${DOCKER_MONITORING}:latest
-	
-	docker build -t ${DOCKER_PROFILE}:${TRAVIS_COMMIT} ../profile-service
-	docker tag -f ${DOCKER_PROFILE}:${TRAVIS_COMMIT} ${DOCKER_PROFILE}:latest && docker push ${DOCKER_PROFILE}:latest
-	
-	docker build -t ${DOCKER_REGISTRY}:${TRAVIS_COMMIT} ../registry
-	docker tag -f ${DOCKER_REGISTRY}:${TRAVIS_COMMIT} ${DOCKER_REGISTRY}:latest && docker push ${DOCKER_REGISTRY}:latest
+	echo $DOCKER_LOGGING:$TAG:$COMMIT
+	docker build -t ${DOCKER_LOGGING}:${COMMIT} ../logging
+	docker tag ${DOCKER_LOGGING}:${COMMIT} ${DOCKER_LOGGING}:${TAG} && docker push ${DOCKER_LOGGING}:${TAG}
+
+	echo $DOCKER_MATCH:$TAG:$COMMIT	
+	docker build -t ${DOCKER_MATCH}:${COMMIT} ../match-service
+	docker tag ${DOCKER_MATCH}:${COMMIT} ${DOCKER_MATCH}:${TAG} && docker push ${DOCKER_MATCH}:${TAG}
+
+	echo $DOCKER_MONITORING:$TAG:$COMMIT		
+	docker build -t ${DOCKER_MONITORING}:${COMMIT} ../monitoring
+	docker tag ${DOCKER_MONITORING}:${COMMIT} ${DOCKER_MONITORING}:${TAG} && docker push ${DOCKER_MONITORING}:${TAG}
+
+	echo $DOCKER_PROFILE:$TAG:$COMMIT	
+	docker build -t ${DOCKER_PROFILE}:${COMMIT} ../profile-service
+	docker tag ${DOCKER_PROFILE}:${COMMIT} ${DOCKER_PROFILE}:${TAG} && docker push ${DOCKER_PROFILE}:${TAG}
+
+	echo $DOCKER_REGISTRY:$TAG:$COMMIT		
+	docker build -t ${DOCKER_REGISTRY}:${COMMIT} ../registry
+	docker tag ${DOCKER_REGISTRY}:${COMMIT} ${DOCKER_REGISTRY}:${TAG} && docker push ${DOCKER_REGISTRY}:${TAG}
 	
 fi
 
