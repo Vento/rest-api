ca289abe828e511b3aabcbf821c55c63a404c06a
vvasiloud <vvasiloudis@gmail.com>
2016-12-31 23:50:30 +0200

    Typo fix

--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -8,7 +8,7 @@ export DOCKER_MATCH=$DOCKER_USERNAME/vento-match
 export DOCKER_MONITORING=$DOCKER_USERNAME/vento-monitoring
 export DOCKER_PROFILE=$DOCKER_USERNAME/vento-profile
 export DOCKER_REGISTRY=$DOCKER_USERNAME/vento-registry
-export COMMIT=${TRAVIS_COMMIT::6}
+export COMMIT=${TRAVIS_COMMIT::7}
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
@@ -16,35 +16,35 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	
 	echo ${DOCKER_AUTH}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_AUTH}:${COMMIT} ../auth-service .
+	docker build -t ${DOCKER_AUTH}:${COMMIT} ${TRAVIS_BUILD_DIR}/auth-service .
 	docker tag ${DOCKER_AUTH}:${COMMIT} ${DOCKER_AUTH}:${TAG} && docker push ${DOCKER_AUTH}:${TAG}
 
 	echo ${DOCKER_CONFIG}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_CONFIG}:${COMMIT} ${$TRAVIS_BUILD_DIR}/config .
+	docker build -t ${DOCKER_CONFIG}:${COMMIT} ${TRAVIS_BUILD_DIR}/config .
 	docker tag ${DOCKER_CONFIG}:${COMMIT} ${DOCKER_CONFIG}:${TAG} && docker push ${DOCKER_CONFIG}:${TAG}
 
 	echo ${DOCKER_GATEWAY}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_GATEWAY}:${COMMIT} ${$TRAVIS_BUILD_DIR}/gateway .
+	docker build -t ${DOCKER_GATEWAY}:${COMMIT} ${TRAVIS_BUILD_DIR}/gateway .
 	docker tag ${DOCKER_GATEWAY}:${COMMIT} ${DOCKER_GATEWAY}:${TAG} && docker push ${DOCKER_GATEWAY}:${TAG}
 
 	echo ${DOCKER_LOGGING}:${TAG}:${COMMIT}
-	docker build -t ${DOCKER_LOGGING}:${COMMIT} ${$TRAVIS_BUILD_DIR}/logging .
+	docker build -t ${DOCKER_LOGGING}:${COMMIT} ${TRAVIS_BUILD_DIR}/logging .
 	docker tag ${DOCKER_LOGGING}:${COMMIT} ${DOCKER_LOGGING}:${TAG} && docker push ${DOCKER_LOGGING}:${TAG}
 
 	echo ${DOCKER_MATCH}:${TAG}:${COMMIT}	
-	docker build -t ${DOCKER_MATCH}:${COMMIT} ${$TRAVIS_BUILD_DIR}/match-service .
+	docker build -t ${DOCKER_MATCH}:${COMMIT} ${TRAVIS_BUILD_DIR}/match-service .
 	docker tag ${DOCKER_MATCH}:${COMMIT} ${DOCKER_MATCH}:${TAG} && docker push ${DOCKER_MATCH}:${TAG}
 
 	echo ${DOCKER_MONITORING}:${TAG}:${COMMIT}		
-	docker build -t ${DOCKER_MONITORING}:${COMMIT} ${$TRAVIS_BUILD_DIR}/monitoring .
+	docker build -t ${DOCKER_MONITORING}:${COMMIT} ${TRAVIS_BUILD_DIR}/monitoring .
 	docker tag ${DOCKER_MONITORING}:${COMMIT} ${DOCKER_MONITORING}:${TAG} && docker push ${DOCKER_MONITORING}:${TAG}
 
 	echo ${DOCKER_PROFILE}:${TAG}:${COMMIT}	
-	docker build -t ${DOCKER_PROFILE}:${COMMIT} ${$TRAVIS_BUILD_DIR}/profile-service .
+	docker build -t ${DOCKER_PROFILE}:${COMMIT} ${TRAVIS_BUILD_DIR}/profile-service .
 	docker tag ${DOCKER_PROFILE}:${COMMIT} ${DOCKER_PROFILE}:${TAG} && docker push ${DOCKER_PROFILE}:${TAG}
 
 	echo ${DOCKER_REGISTRY}:${TAG}:${COMMIT}		
-	docker build -t ${DOCKER_REGISTRY}:${COMMIT} ${$TRAVIS_BUILD_DIR}/registry .
+	docker build -t ${DOCKER_REGISTRY}:${COMMIT} ${TRAVIS_BUILD_DIR}/registry .
 	docker tag ${DOCKER_REGISTRY}:${COMMIT} ${DOCKER_REGISTRY}:${TAG} && docker push ${DOCKER_REGISTRY}:${TAG}
 	
 fi
