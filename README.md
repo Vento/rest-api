6355dc35ac877457006925a3d4ef9a6f80436b6a
vvasiloud <vvasiloudis@gmail.com>
2017-10-25 20:58:52 +0200

    ELK Dockerfiles updates

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 edu-microservices
 
-[![Build Status](https://travis-ci.com/vvasiloud/edu-microservices.svg?token=pkGRxtpAqnsxd33rFipf&branch=master)](https://travis-ci.com/vvasiloud/edu-microservices) [![codecov](https://codecov.io/gh/vvasiloud/edu-microservices/branch/master/graph/badge.svg?token=anhh0mxPWv)](https://codecov.io/gh/vvasiloud/edu-microservices) [![Dependency Status](https://www.versioneye.com/user/projects/5813a79acd069a3418079fa0/badge.svg)](https://www.versioneye.com/user/projects/5813a79acd069a3418079fa0?child=summary#dialog_dependency_badge)
+[![Build Status](https://api.travis-ci.org/vvasiloud/edu-microservices.svg?branch=master)](https://travis-ci.org/vvasiloud/edu-microservices) [![codecov](https://codecov.io/gh/vvasiloud/edu-microservices/branch/master/graph/badge.svg?token=anhh0mxPWv)](https://codecov.io/gh/vvasiloud/edu-microservices) [![Dependency Status](https://www.versioneye.com/user/projects/5813a79acd069a3418079fa0/badge.svg)](https://www.versioneye.com/user/projects/5813a79acd069a3418079fa0?child=summary#dialog_dependency_badge)
 
 
--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -3,7 +3,9 @@
 export DOCKER_AUTH=$DOCKER_USERNAME/vento-auth
 export DOCKER_CONFIG=$DOCKER_USERNAME/vento-config
 export DOCKER_GATEWAY=$DOCKER_USERNAME/vento-gateway
-export DOCKER_LOGGING=$DOCKER_USERNAME/vento-logging
+export DOCKER_ELASTICSEARCH=$DOCKER_USERNAME/vento-elasticsearch
+export DOCKER_KIBANA=$DOCKER_USERNAME/vento-kibana
+export DOCKER_LOGSTASH=$DOCKER_USERNAME/vento-logstash
 export DOCKER_MATCH=$DOCKER_USERNAME/vento-match
 export DOCKER_MONITORING=$DOCKER_USERNAME/vento-monitoring
 export DOCKER_PROFILE=$DOCKER_USERNAME/vento-profile
@@ -31,10 +33,20 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker build -t ${DOCKER_GATEWAY} .
 	docker tag ${DOCKER_GATEWAY}:${TAG} && docker push ${DOCKER_GATEWAY}:${TAG}
 
-	#echo ${DOCKER_LOGGING}:${TAG}
-	#cd ${TRAVIS_BUILD_DIR}/logging
-	#docker build -t ${DOCKER_LOGGING} .
-	#docker tag ${DOCKER_LOGGING}:${TAG} && docker push ${DOCKER_LOGGING}:${TAG}
+	echo ${DOCKER_ELASTICSEARCH}:${TAG}
+	cd ${TRAVIS_BUILD_DIR}/logging/elasticsearch
+	docker build -t ${DOCKER_ELASTICSEARCH} .
+	docker tag ${DOCKER_ELASTICSEARCH}:${TAG} && docker push ${DOCKER_ELASTICSEARCH}:${TAG}
+
+	echo ${DOCKER_KIBANA}:${TAG}
+	cd ${TRAVIS_BUILD_DIR}/logging/kibana
+	docker build -t ${DOCKER_KIBANA} .
+	docker tag ${DOCKER_KIBANA}:${TAG} && docker push ${DOCKER_KIBANA}:${TAG}
+
+	echo ${DOCKER_LOGSTASH}:${TAG}
+	cd ${TRAVIS_BUILD_DIR}/logging/logstash
+	docker build -t ${DOCKER_LOGSTASH} .
+	docker tag ${DOCKER_LOGSTASH}:${TAG} && docker push ${DOCKER_LOGSTASH}:${TAG}
 
 	echo ${DOCKER_MATCH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/match-service
