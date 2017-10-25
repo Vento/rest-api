43795254df57a20ab43522fdd5063cde75e7125c
vvasiloud <vvasiloudis@gmail.com>
2017-10-25 21:22:17 +0200

    Updated docker build script

--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -20,58 +20,58 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	
 	echo ${DOCKER_AUTH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/auth-service
-	docker build -t ${DOCKER_AUTH} .
-	docker tag ${DOCKER_AUTH}:${TAG} && docker push ${DOCKER_AUTH}:${TAG}
+	docker build -t ${DOCKER_AUTH}:${TAG} .
+	docker push ${DOCKER_AUTH}:${TAG}
 
 	echo ${DOCKER_CONFIG}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/config
-	docker build -t ${DOCKER_CONFIG} .
-	docker tag ${DOCKER_CONFIG}:${TAG} && docker push ${DOCKER_CONFIG}:${TAG}
+	docker build -t ${DOCKER_CONFIG}:${TAG} .
+	docker push ${DOCKER_CONFIG}:${TAG}
 
 	echo ${DOCKER_GATEWAY}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/gateway
-	docker build -t ${DOCKER_GATEWAY} .
-	docker tag ${DOCKER_GATEWAY}:${TAG} && docker push ${DOCKER_GATEWAY}:${TAG}
+	docker build -t ${DOCKER_GATEWAY}:${TAG} .
+	docker push ${DOCKER_GATEWAY}:${TAG}
 
 	echo ${DOCKER_ELASTICSEARCH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/elasticsearch
-	docker build -t ${DOCKER_ELASTICSEARCH} .
-	docker tag ${DOCKER_ELASTICSEARCH}:${TAG} && docker push ${DOCKER_ELASTICSEARCH}:${TAG}
+	docker build -t ${DOCKER_ELASTICSEARCH}:${TAG} .
+	docker push ${DOCKER_ELASTICSEARCH}:${TAG}
 
 	echo ${DOCKER_KIBANA}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/kibana
-	docker build -t ${DOCKER_KIBANA} .
-	docker tag ${DOCKER_KIBANA}:${TAG} && docker push ${DOCKER_KIBANA}:${TAG}
+	docker build -t ${DOCKER_KIBANA}:${TAG} .
+	docker push ${DOCKER_KIBANA}:${TAG}
 
 	echo ${DOCKER_LOGSTASH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/logstash
-	docker build -t ${DOCKER_LOGSTASH} .
-	docker tag ${DOCKER_LOGSTASH}:${TAG} && docker push ${DOCKER_LOGSTASH}:${TAG}
+	docker build -t ${DOCKER_LOGSTASH}:${TAG} .
+	docker push ${DOCKER_LOGSTASH}:${TAG}
 
 	echo ${DOCKER_MATCH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/match-service
-	docker build -t ${DOCKER_MATCH} .
-	docker tag ${DOCKER_MATCH}:${TAG} && docker push ${DOCKER_MATCH}:${TAG}
+	docker build -t ${DOCKER_MATCH}:${TAG} .
+	docker push ${DOCKER_MATCH}:${TAG}
 
 	echo ${DOCKER_MONITORING}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/monitoring
-	docker build -t ${DOCKER_MONITORING} .
-	docker tag ${DOCKER_MONITORING}:${TAG} && docker push ${DOCKER_MONITORING}:${TAG}
+	docker build -t ${DOCKER_MONITORING}:${TAG} .
+	docker push ${DOCKER_MONITORING}:${TAG}
 
 	echo ${DOCKER_PROFILE}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/profile-service
-	docker build -t ${DOCKER_PROFILE} .
-	docker tag ${DOCKER_PROFILE}:${TAG} && docker push ${DOCKER_PROFILE}:${TAG}
+	docker build -t ${DOCKER_PROFILE}:${TAG} .
+	docker push ${DOCKER_PROFILE}:${TAG}
 
 	echo ${DOCKER_REGISTRY}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/registry
-	docker build -t ${DOCKER_REGISTRY} .
-	docker tag ${DOCKER_REGISTRY}:${TAG} && docker push ${DOCKER_REGISTRY}:${TAG}
+	docker build -t ${DOCKER_REGISTRY}:${TAG} .
+	docker push ${DOCKER_REGISTRY}:${TAG}
 
 	echo ${DOCKER_TRACING}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/tracing
-	docker build -t ${DOCKER_TRACING} .
-	docker tag ${DOCKER_TRACING}:${TAG} && docker push ${DOCKER_TRACING}:${TAG}
+	docker build -t ${DOCKER_TRACING}:${TAG} .
+	docker push ${DOCKER_TRACING}:${TAG}
 
 	cd ${TRAVIS_BUILD_DIR}
 fi
