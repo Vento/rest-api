7dbc3610e9c0b64186b1b42bb34328cc23a36c9d
vvasiloud <vvasiloudis@gmail.com>
2017-11-04 21:26:33 +0100

    Docker build script update

--- scripts/docker.build.sh
+++ scripts/docker.build.sh
@@ -16,61 +16,71 @@ export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 	docker --version
-	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-	
+
 	echo ${DOCKER_AUTH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/auth-service
 	docker build -t ${DOCKER_AUTH}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_AUTH}:${TAG}
 
 	echo ${DOCKER_CONFIG}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/config
 	docker build -t ${DOCKER_CONFIG}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_CONFIG}:${TAG}
 
 	echo ${DOCKER_GATEWAY}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/gateway
 	docker build -t ${DOCKER_GATEWAY}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_GATEWAY}:${TAG}
 
 	echo ${DOCKER_ELASTICSEARCH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/elasticsearch
 	docker build -t ${DOCKER_ELASTICSEARCH}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_ELASTICSEARCH}:${TAG}
 
 	echo ${DOCKER_KIBANA}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/kibana
 	docker build -t ${DOCKER_KIBANA}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_KIBANA}:${TAG}
 
 	echo ${DOCKER_LOGSTASH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/logging/logstash
 	docker build -t ${DOCKER_LOGSTASH}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_LOGSTASH}:${TAG}
 
 	echo ${DOCKER_MATCH}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/match-service
 	docker build -t ${DOCKER_MATCH}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_MATCH}:${TAG}
 
 	echo ${DOCKER_MONITORING}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/monitoring
 	docker build -t ${DOCKER_MONITORING}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_MONITORING}:${TAG}
 
 	echo ${DOCKER_PROFILE}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/profile-service
 	docker build -t ${DOCKER_PROFILE}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_PROFILE}:${TAG}
 
 	echo ${DOCKER_REGISTRY}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/registry
 	docker build -t ${DOCKER_REGISTRY}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_REGISTRY}:${TAG}
 
 	echo ${DOCKER_TRACING}:${TAG}
 	cd ${TRAVIS_BUILD_DIR}/tracing
 	docker build -t ${DOCKER_TRACING}:${TAG} .
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 	docker push ${DOCKER_TRACING}:${TAG}
 
 	cd ${TRAVIS_BUILD_DIR}
